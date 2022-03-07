868aa28a5f186b6e0bb7681b64524fe1f2ea9ffd
jeremy <jeremypietila@gmail.com>
2022-03-07 15:08:11 -0800

    any & all

--- enumerable.rb
+++ enumerable.rb
@@ -1,17 +1,17 @@
 class Array
 
-    def my_each(blc)
+    def my_each(&blc)
         result = []
         counter = 0
 
         while counter < self.length
-            result[counter] << blc.call(self[counter])
+            result << blc.call(self[counter])
             counter += 1 
         end
         return result
     end
 
-    def my_select(blc)
+    def my_select(&blc)
         result = []
         self.my_each do |elem|
             if blc.call(elem)
@@ -21,7 +21,7 @@ class Array
         return result
     end
 
-    def my_reject(blc)
+    def my_reject(&blc)
         result = []
         self.my_each do |elem|
             if !blc.call(elem)
@@ -30,5 +30,20 @@ class Array
         end
         return result
     end
+
+    def my_any?(&blc)
+        self.my_each do |elem|
+            return true if blc.call(elem)
+        end
+        return false
+    end
+
+    def my_all?(&blc)
+        self.my_each do |elem|
+            return false if !blc.call(elem)
+        end
+        return true
+    end
+
 
 end
