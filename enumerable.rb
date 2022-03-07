320562a11fff527eede549a896ab085981a2ae83
echin522 <echin522@gmail.com>
2022-03-07 14:50:40 -0800

    fixing stuff

---
+++ enumerable.rb
@@ -0,0 +1,26 @@
+class Array
+
+    def my_each(blc)
+        result = []
+        counter = 0
+
+        while counter < self.length
+            result[counter] << blc.call(self[counter])
+            counter += 1 
+        end
+        return result
+    end
+
+    def my_select(blc)
+        result = []
+        self.my_each do |elem|
+            if blc.call(elem)
+                result << elem
+            end
+        end
+        return result
+    end
+
+    
+
+end
