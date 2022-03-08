c9068624f73d200b81fead3d500b4cedf00d0387
jeremy <jeremypietila@gmail.com>
2022-03-07 16:36:42 -0800

    zip + rotate

--- array.rb
+++ array.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class Array
     def my_flatten
         result =[]
@@ -10,4 +12,38 @@ class Array
         end
         return result
     end
+
+    def my_zip(*arrs)
+        grid = Array.new(arrs.length+1) {Array.new(self.length) }
+        my_arrs = [self,*arrs]
+
+        my_arrs.each_with_index do |row,i|
+            row.each_with_index do |elem,j|
+                # debugger
+                grid[j][i] = elem
+            end
+        end
+        return grid[0...self.length]
+    end
+
+
+    def my_rotate(num=1)
+        result = self
+        while num != 0
+            if num < 0
+                result = [result[-1]] + result[0...result.length-1]
+                num += 1
+            else
+                result = result[1..result.length] + [result[0]]
+                num -= 1
+            end
+        end
+        return result
+    end
+
+
+
+
+
+
 end
